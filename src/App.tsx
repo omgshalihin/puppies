f590a3693b25b62ee1d5e6e453f1fa4a9910bd14
omgshalihin <shalihinsgp@gmail.com>
2023-01-12 17:32:10 +0100

    first attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState()
+  const [error, setError] = useState<string>()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://puppies-api-production.up.railway.app/api/puppies')
+        const newData = await response.json()
+        setData(newData)
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (err: any) {
+        setError(err.message)
+      }
+    }
+    fetchData()
+  }, [])
+
+  console.log(data)
+
+  if (data === undefined) return <h1>Loading Main Page...</h1>
+
+  if (error) return <h1>Please look at this error: {error}</h1>
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <header className='app-header'>hey</header>
     </div>
   )
 }
