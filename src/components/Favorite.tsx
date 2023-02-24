796b372e4a303b8c94b0e89c2ce67f3e5743d229
omgshalihin <shalihinsgp@gmail.com>
2023-02-24 14:27:31 +0100

    padding of 2em applied to favorites

--- src/components/Favorite.tsx
+++ src/components/Favorite.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 import React from 'react'
+import './Favorite.css'
 
 const Favorite = ({ puppies }: any) => {
   const onlyFavPuppies = puppies.filter((el: any) => el.favorite === 'true')
@@ -7,43 +8,45 @@ const Favorite = ({ puppies }: any) => {
   return (
     <>
       <h1 className='header'>My Favorite Puppies</h1>
-      {onlyFavPuppies.map(
-        (el: {
-          name:
-            | string
-            | number
-            | boolean
-            | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-            | React.ReactFragment
-            | React.ReactPortal
-            | null
-            | undefined
-          dob:
-            | string
-            | number
-            | boolean
-            | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-            | React.ReactFragment
-            | React.ReactPortal
-            | null
-            | undefined
-          breed:
-            | string
-            | number
-            | boolean
-            | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-            | React.ReactFragment
-            | React.ReactPortal
-            | null
-            | undefined
-        }) => (
-          <>
-            <p>
-              {el.name} is a {el.breed} breed and born on the {el.dob}.
-            </p>
-          </>
-        ),
-      )}
+      <div className='fav-content'>
+        {onlyFavPuppies.map(
+          (el: {
+            name:
+              | string
+              | number
+              | boolean
+              | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+              | React.ReactFragment
+              | React.ReactPortal
+              | null
+              | undefined
+            dob:
+              | string
+              | number
+              | boolean
+              | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+              | React.ReactFragment
+              | React.ReactPortal
+              | null
+              | undefined
+            breed:
+              | string
+              | number
+              | boolean
+              | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+              | React.ReactFragment
+              | React.ReactPortal
+              | null
+              | undefined
+          }) => (
+            <>
+              <p>
+                {el.name} is a {el.breed} breed and born on the {el.dob}.
+              </p>
+            </>
+          ),
+        )}
+      </div>
     </>
   )
 }
